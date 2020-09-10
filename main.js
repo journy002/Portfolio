7cc7045268e2e0fa07ae01c25449f0d99f52b2b4
munseok <journy002@naver.com>
2020-09-10 14:28:17 +0900

    JS: Navbar Menu click

--- main.js
+++ main.js
@@ -4,10 +4,23 @@
 // 제일 위 목록 고정 상단바 밑 배경 만드는 코드.
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
+})
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
 })
