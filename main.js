7ae039cc0dac06418c8065881cf7ab5c53f974cc
munseok <journy002@naver.com>
2020-09-10 16:49:14 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -31,16 +31,27 @@ contactMe.addEventListener('click', () => {
 });
 
 // Make home slowly fade to transparent as the window scrolls down
+// 스크롤을 통해 화면을 내리면 home__container에 있는 글자들의 투명도가 낮아진다.
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height //높이 구하기
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
 
-
-
-
+//Handle click on the "Arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
 
 // 중복되는 스크롤 코드가 있기에 스크롤 함수를 하나 만들었다.
 // (selector)를 넣어주면 scrollIntoView() <- 괄호안에 원하는 셀렉터를 주면 된다.
@@ -48,3 +59,6 @@ function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+
+
